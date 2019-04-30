b13d4cf7b437a24354d8e51f258d809163d5cf2d
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-04-30 01:23:18 -0300

    Adicionando funções básicas do firebase para o banco de pessoas que segui e de pessoas que desegui

--- Firebase/index.js
+++ Firebase/index.js
@@ -1,9 +1,29 @@
 require('dotenv').config({ path: '.env' })
-const admin = require('firebase-admin')
-const serviceAccount = require(process.env.FIREBASE_DB_KEY)
+const firebase = require('firebase-admin')
+const config = require(process.env.FIREBASE_DB_KEY)
 
+module.exports = {
+  addFollowing,
+  getFollowings,
+  unFollow,
+  inHistory
+}
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+firebase.initializeApp({
+  credential: firebase.credential.cert(config),
   databaseURL: process.env.FIREBASE_DB_KEY //database_config.json, arquivo que baixamos no firebase
 })
+
+const database = firebase.database()
+
+const following = (param = '') => database.ref(`following/${param}`);
+
+const followHistory = (param = '') => database.ref(`follow_history/${param}`);
+
+const addFollowing = async username => following(username).set({ username, added: (new Date().getTime()) })
+
+const getFollowings = () => following().once('value').then(data => data.val())
+
+const unFollow = username => following(username).remove().then(() => followHistory(username).set({ username }))
+
+const inHistory = async username => followHistory(username).once('value').then(data => data.val())
